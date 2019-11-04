7b514ff25e8e0a2b0483e447ae7967a2595eefc1
luzhongkuan <luzhongk@126.com>
2019-11-04 15:59:40 +0800

    docs: changlog

--- packages/vue/CHANGELOG.md
+++ packages/vue/CHANGELOG.md
@@ -1,3 +1,7 @@
+## [0.0.2](https://github.com/kuan1/luzhongk/compare/v0.0.2-alpha.0...v0.0.2) (2019-11-04)
+
+
+
 ## [0.0.2-alpha.0](https://github.com/kuan1/luzhongk/compare/v0.0.1...v0.0.2-alpha.0) (2019-11-04)
 
 
--- packages/vue/index.js
+++ packages/vue/index.js
@@ -2,8 +2,3 @@ import { loading, toast } from '@luzhongk/vue-feedback'
 import { createApi, encrypt } from '@luzhongk/vue-request'
 
 export { loading, toast, createApi, encrypt }
-
-// export {
-//   loading,
-//   toast
-// }
