da8faf89982cf868c2c11bdd96b0dba91637e007
luzhongkuan <luzhongk@126.com>
2019-11-05 10:01:39 +0800

    chore: vuepress 配置

--- docs/.vuepress/mapPackage.js
+++ docs/.vuepress/mapPackage.js
@@ -2,13 +2,13 @@ const fs = require('fs')
 const path = require('path')
 
 function resolve(dir) {
-  return path.resolve(__dirname, '../..', dir)
+  return path.resolve(__dirname, '..', '..', dir)
 }
 
 function mapPackage() {
   const dirList = fs.readdirSync(resolve('packages'))
   return dirList.reduce((obj = {}, name) => {
-    obj[`@luzhongk/${name}`] = resolve(`/packages/${name}/index.vue`)
+    obj[`@luzhongk/${name}`] = resolve(`packages/${name}/index.vue`)
     return obj
   }, {})
 }
