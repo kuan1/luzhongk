fc119b1be49b5a693cb41959c27c8cc6115645a4
luzhongkuan <luzhongk@126.com>
2019-11-08 22:12:47 +0800

    chore: vue-drawer

--- packages/vue-drawer/src/ToggleBar.vue
+++ packages/vue-drawer/src/ToggleBar.vue
@@ -1,5 +1,5 @@
 <template>
-  <div :class="{active: dataActive}" @click="handleClick" class="nav-toggle-bar">
+  <div :class="{active: active}" @click="handleClick" class="nav-toggle-bar">
     <span class="nav-bar-item"></span>
     <span class="nav-bar-item"></span>
     <span class="nav-bar-item"></span>
@@ -18,21 +18,56 @@ export default {
       default: false
     }
   },
-  data() {
-    return {
-      dataActive: this.active
-    }
-  },
-  watch: {
-    active(active) {
-      this.dataActive = active
-    }
-  },
   methods: {
     handleClick() {
-      this.dataActive = !this.dataActive
-      this.$emit('click', this.dataActive)
+      this.$emit('click', !this.active)
     }
   }
 }
 </script>
+
+<style lang="less" scoped>
+@font-color: #333;
+@toggle-bar-height: 16px;
+
+.nav-toggle-bar {
+  width: @toggle-bar-height + 4px;
+  height: @toggle-bar-height;
+  position: relative;
+  opacity: 0.6;
+  cursor: pointer;
+  &.active .nav-bar-item {
+    &:nth-child(1) {
+      transform: rotate(45deg);
+    }
+    &:nth-child(2) {
+      opacity: 0;
+    }
+    &:nth-child(3) {
+      transform: rotate(-45deg);
+    }
+  }
+  .nav-bar-item {
+    background: @font-color;
+    position: absolute;
+    left: 0;
+    width: 100%;
+    height: 2px;
+    transition: 0.4s;
+    transform-origin: 0;
+    border-radius: 2px;
+    transform: rotate(0);
+    opacity: 1;
+    &:nth-child(1) {
+      top: 0;
+    }
+    &:nth-child(2) {
+      top: (@toggle-bar-height - 2px) / 2;
+    }
+    &:nth-child(3) {
+      top: @toggle-bar-height - 2px;
+    }
+  }
+}
+</style>
+
--- packages/vue-drawer/src/VueDrawer.vue
+++ packages/vue-drawer/src/VueDrawer.vue
@@ -1,22 +1,17 @@
 <template>
   <div class="vue-drawer-container">
     <div class="vue-drawer-header">
-      <toggle-bar v-model="active"/>
+      <toggle-bar class="toggle-bar" v-model="active"/>
     </div>
-    <drawer-box v-model="active"/>
   </div>
 </template>
 
 <script>
 import ToggleBar from './ToggleBar.vue'
-import DrawerBox from './DrawerBox.vue'
-
-import './style.less'
 
 export default {
   components: {
     ToggleBar,
-    DrawerBox
   },
   data() {
     return {
@@ -25,3 +20,23 @@ export default {
   }
 }
 </script>
+
+<style lang="less" scoped>
+@mobile-width: 768px;
+
+.vue-drawer-header {
+  height: 50px;
+  padding: 0 20px;
+  display: flex;
+  align-items: center;
+  color: rgba(255, 255, 255, 0.7);
+  box-shadow: 0 1px 5px rgba(0, 0, 0, 0.12);
+}
+
+// 大屏幕
+@media screen and (min-width: @mobile-width) {
+  .toggle-bar {
+    display: none;
+  }
+}
+</style>
