9d6c1feaae4f32ee65ceebd461d7e5f5ecf94e87
luzhongkuan <luzhongk@126.com>
2019-11-11 12:11:25 +0800

    chore: build

--- packages/vue-debug/lib/vue-debug.umd.js
+++ packages/vue-debug/lib/vue-debug.umd.js
@@ -4,9 +4,9 @@
 	else if(typeof define === 'function' && define.amd)
 		define([], factory);
 	else if(typeof exports === 'object')
-		exports["vue-feedback"] = factory();
+		exports["vue-debug"] = factory();
 	else
-		root["vue-feedback"] = factory();
+		root["vue-debug"] = factory();
 })((typeof self !== 'undefined' ? self : this), function() {
 return /******/ (function(modules) { // webpackBootstrap
 /******/ 	// The module cache
